67a3d1ce6801920eb3d361b2d2c6e9ba2ce78883
Jake Sills <jsills2592@gmail.com>
2025-01-23 15:10:07 -0500

    Added the buttons display

---
+++ src/Components/Display.js
@@ -0,0 +1,14 @@
+<div id="drum-machine">
+  <div id="display"></div>
+  <div class="drum-pad-container">
+    <button class="drum-pad" id="Heater-1">Q</button>
+    <button class="drum-pad" id="Heater-2">W</button>
+    <button class="drum-pad" id="Heater-3">E</button>
+    <button class="drum-pad" id="Heater-4">A</button>
+    <button class="drum-pad" id="Clap">S</button>
+    <button class="drum-pad" id="Open-HH">D</button>
+    <button class="drum-pad" id="Kick-n'-Hat">Z</button>
+    <button class="drum-pad" id="Kick">X</button>
+    <button class="drum-pad" id="Closed-HH">C</button>
+  </div>
+</div>
--- src/Components/DrumMachine.js
+++ src/Components/DrumMachine.js
@@ -1,11 +1,32 @@
-import react from 'react';
+const drumPads = [
+    { key: "Q", id: "Heater-1" },
+    { key: "W", id: "Heater-2" },
+    { key: "E", id: "Heater-3" },
+    { key: "A", id: "Heater-4" },
+    { key: "S", id: "Clap" },
+    { key: "D", id: "Open-HH" },
+    { key: "Z", id: "Kick-n'-Hat" },
+    { key: "X", id: "Kick" },
+    { key: "C", id: "Closed-HH" },
+];
 
 const DrumMachine = () => {
-return (
-    <div id="drum-machine">
-        <h1>Drum Machine</h1>
-    </div>
-)
+    return (
+        <div id="drum-machine">
+            <div id="display">DRUM MACHINE</div>
+            <div className="drum-pad-container">
+                {drumPads.map((pad) => (
+                    <button
+                        className="drum-pad"
+                        id={pad.id}
+                        key={pad.key}
+                    >
+                        {pad.key}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default DrumMachine;
